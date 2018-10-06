679c900cd952eb0e3769837d0ff9d32b1b3d4ec2
Edward Kang <edk926@ice1.fas.harvard.edu>
2018-10-06 13:59:51 -0400

    part 4 4th try some works?

--- tt2ht2.c
+++ tt2ht2.c
@@ -4,11 +4,13 @@
 int
 main()
 {
+	/*
 	int c, d;
 	int new_line = 0;
 	int first_td = 0;
 	int current_state = 0;
 
+	
 	char table1[8];
 	char table2[8];
 	char tr1[5];
@@ -22,14 +24,48 @@ main()
 	strcpy(tr2, "</tr>");
 	strcpy(td1, "<td>");
 	strcpy(td2, "</td>");
+	*/
 
-	char basic_array[11];
+	int c;
+	int current_state = 0;
+	char basic_array[12];
 	int i = 0;
-
+	
 	while( (c=getchar()) != EOF )
 	{
 		basic_array[i] = c;
 		i++;
-		puts(basic_array);
+		basic_array[i+1] = '\0';
+
+		//puts(basic_array);
+
+		if ( strcmp(basic_array, "<noprocess>" ) == 0 )
+		{
+			current_state = 1;
+		} else if ( strcmp(basic_array, "</noprocess>" ) == 0 )
+		{
+			current_state = 0;
+		}
+
+		if ( c == '\n')
+		{
+			puts(basic_array);
+		}
+
+		/*
+		if (current_state == 1)
+		{
+			puts(basic_array);
+		}		
+		
+		if (current_state == 0)
+		{
+			if ( c == '\n')
+			{
+				puts(basic_array);
+				basic_array = { 0 };
+			}
+		}
+		*/
 	}	
 }
