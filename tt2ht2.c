24adf4dd57ba2848f94276239be07f3d6ed68761
Edward Kang <edk926@ice1.fas.harvard.edu>
2018-10-07 01:58:54 -0400

    part 4 done

--- tt2ht2.c
+++ tt2ht2.c
@@ -18,17 +18,16 @@
 
 
 #define	CONVERT_ROW	0	// convert the row into html table row
-#define	NOPROCESS_PRINT	1
-#define	ATTRIBUTES	2
-#define	END_OF_ATTRIBS	3
+#define	NOPROCESS_PRINT	1	// print line
+#define	ATTRIBUTES	2	// store line as an attribute
 
-#define	START_LINE	0
-#define	START_COL	1
-#define	PRINT_DATA	2
+#define	START_LINE	0	// start table row with <tr> <td>
+#define	START_COL	1	// start table row with <td>
+#define	PRINT_DATA	2	// print char for <td>
 
-#define	LINELEN		500
-#define ATTRINUM	50
-#define ATTRILEN	200
+#define	LINELEN		500	// length of each lines
+#define ATTRINUM	50	// number of attribute lines
+#define ATTRILEN	200	// length of each attribute lines
 
 #define	TR1		"<tr>"
 #define	TR2		"</tr>"
@@ -38,8 +37,9 @@
 
 void	process_input();
 int	print_line(char [], int );
-void	convert_row(char[], char*);
-//void	get_attribute(char[]);
+void	convert_row(char[], char[][ATTRILEN]);
+
+int	i = 0;	// counter for string array of attributes in print_line method
 
 int
 main()
@@ -56,25 +56,24 @@ main()
  */
 void process_input()
 {
-	int current_state = CONVERT_ROW;
-	char basic_array[LINELEN];
+	int current_state = CONVERT_ROW;	// state of current lines
+	char basic_array[LINELEN];		// array for current line
 	
-	/* we read in a b c */
 	while( fgets(basic_array, LINELEN, stdin ) )
 	{
 		// check for mode changes
 		if ( strcmp(basic_array, "<noprocess>\n") == 0 )
 		{
-			// when we see <noprocess, change mode and print line
+			// when we see <noprocess, change state to noprocess and just printout the line with no conversion
 			current_state = NOPROCESS_PRINT;
 		} else if ( strcmp(basic_array, "</noprocess>\n") == 0 )
-		{
+		{	// when we see closing noprocess tab, change state to convert row
 			current_state = CONVERT_ROW;
 		} else if ( strcmp(basic_array, "<attributes>\n") == 0 )
-		{
+		{	// when we see attribtes, change state to attributes tab
 			current_state = ATTRIBUTES;
 		} else if ( strcmp(basic_array, "</attributes>\n") == 0 )
-		{
+		{	// when we see closing attributes tab, change state to convert row
 			current_state = CONVERT_ROW;
 		}
 		// if not a tag, process as data or as noprocess raw text
@@ -87,28 +86,27 @@ void process_input()
 /*
  * print_line
  *   purp: called when we print out a line
- *	   used when state is NOPROCESS_PRINT or CONVERT_ROW
+ *	   used when state is NOPROCESS_PRINT, ATTRIBUTES, or CONVERT_ROW
  *   args: the input line, currnet state
- *   does: checks mode and either just prints the line or converts
+ *   does: checks mode and either just prints the line, converts, or store attributes in string array
  *   rets: 0 for now
  */
 int print_line(char basic_array[], int current_state)
 {
-	int i = 0;
-	char attribute_array[ATTRINUM][ATTRILEN];
+	static char attribute_array[ATTRINUM][ATTRILEN];	// string array for attributes
 
 	if ( current_state == NOPROCESS_PRINT )
-	{
+	{	// noprocess, just print the line with no conversion
 		puts(basic_array);
 	} else if ( current_state == CONVERT_ROW )
-	{	
-		convert_row(basic_array, &attribute_array);
-		//convert_row(basic_array);
+	{	// convert row, convert it into HTML table form then print out
+		convert_row(basic_array, attribute_array);
 	} else if ( current_state == ATTRIBUTES )
-	{
+	{	// store each line as an attribute string array
 		strcpy(attribute_array[i], basic_array);
-		//puts(attribute_array[i]);
+		i++;
 	}
+
 	return 0;
 }
 
@@ -118,47 +116,44 @@ int print_line(char basic_array[], int current_state)
  *	rets: nothing
  *	does: looks for spaces and inserts <td> <tr> etc
  */
-void convert_row(char basic_array[], char *atribute_array)
+void convert_row(char basic_array[], char attribute_array[][ATTRILEN])
 {
-	int	line_state = START_LINE;
-	int	j;
-	int	td_count = 0;
-	
-	//puts(attribute_array[0]);
-	//puts(attribute_array[2]);
+	int	line_state = START_LINE;	// state for each column
+	int	j;				// counter for each char of the line
+	int	td_count = 0;			// counter to follow the number of columns
 
 	for ( j=0; j<strlen(basic_array) ; j++)
 	{
-		if (line_state == START_LINE)
+		if (line_state == START_LINE)	// when it begins, start with <tr> tag then <td> with attribute
 		{
-			puts(TR1);
-			puts(TD1);
-			//puts(attribute_array[td_count]);
-			puts(TD2);
-			putchar(basic_array[j]);
+			puts(TR1);			// <tr>
+			puts(TD1);			// <td
+			puts(attribute_array[td_count]);// class='tty' for example
+			puts(TD2);			// >
+			putchar(basic_array[j]);	// then print out each chars
 			line_state = PRINT_DATA;								
-			//td_count++;
-		} else if (line_state == START_COL)
+			td_count++;
+		} else if (line_state == START_COL)	// not the beginning of the line, but column
 		{
-			puts(TD1);
-			//puts(attribute_array[td_count]);
+			puts(TD1);			// print out <td> tag only
+			puts(attribute_array[td_count]);
 			puts(TD2);
 			putchar(basic_array[j]);
 			line_state = PRINT_DATA;
-			//td_count++;
-		} else if (line_state == PRINT_DATA)
+			td_count++;
+		} else if (line_state == PRINT_DATA)	// keep printing char
 		{
 			putchar(basic_array[j]);
-			if ( isspace(basic_array[j]) )
+			if ( isspace(basic_array[j]) )	// check if it hits any space char
 			{
-				if ( basic_array[j] == '\n')
+				if ( basic_array[j] == '\n')	// if it is the end of the row, close tr tag
 				{
 					puts(TD3);
 					puts(TR2);
 					line_state = START_LINE;
 
 				} else
-				{
+				{			// other than that, just close td tag only
 					puts(TD3);
 					line_state = START_COL;
 				}
